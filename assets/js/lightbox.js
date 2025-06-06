57079756ac2530588b5736c2011d8d35edfd6618
Macintosh HD <anitakonkova@gmail.com>
2025-06-06 09:21:43 -0700

    lightbox scroll revision

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -1,23 +1,34 @@
-
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelectorAll('img[id$="-thumb"]').forEach(img => {
     const id = img.id.replace('-thumb', '');
     const lightbox = document.getElementById(`${id}-lightbox`);
     const close = document.getElementById(`${id}-close`);
 
+    const lockScroll = () => {
+      document.body.style.overflow = 'hidden';
+    };
+
+    const unlockScroll = () => {
+      document.body.style.overflow = '';
+    };
+
     img.addEventListener('click', () => {
       lightbox.style.display = 'flex';
+      lockScroll();
     });
 
     close.addEventListener('click', () => {
       lightbox.style.display = 'none';
+      unlockScroll();
     });
 
     lightbox.addEventListener('click', e => {
       if (e.target === lightbox) {
         lightbox.style.display = 'none';
+        unlockScroll();
       }
     });
   });
 });
+
 
